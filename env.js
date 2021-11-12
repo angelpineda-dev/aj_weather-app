386ddca11eae5c5756d1f4e1577fbc30d406e05c
Angel_Pineda <angeljpa95@gmail.com>
2021-11-12 12:41:12 -0600

    Updates

---
+++ env.js
@@ -0,0 +1,3 @@
+window.env = {
+  "WEATHER_KEY": "01168815b8c9eb4392f3c3208532cbfd"
+};
---
+++ precache-manifest.54b8b1eefad58765145d7d917edb4b5e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b66ab9cab5556841950c3c04d0dcf74",
+    "url": "/aj_weather-app/index.html"
+  },
+  {
+    "revision": "8b6ca3f9b9d68769c7c9",
+    "url": "/aj_weather-app/static/css/main.224268e3.chunk.css"
+  },
+  {
+    "revision": "fee496f1ca593ce11779",
+    "url": "/aj_weather-app/static/js/2.9f672796.chunk.js"
+  },
+  {
+    "revision": "85823014ddcd95089b403f250aeea687",
+    "url": "/aj_weather-app/static/js/2.9f672796.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b6ca3f9b9d68769c7c9",
+    "url": "/aj_weather-app/static/js/main.f799c2b0.chunk.js"
+  },
+  {
+    "revision": "6193fb8a3bfb6bbea5af",
+    "url": "/aj_weather-app/static/js/runtime-main.864a3a0b.js"
+  }
+]);
